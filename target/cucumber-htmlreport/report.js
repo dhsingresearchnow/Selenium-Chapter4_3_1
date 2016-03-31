294e2e40368a3161d466a8e3888251cde126bf88
D Hsing <dhsing@researchnow.com>
2016-03-31 14:33:10 -0500

    Initial version

---
+++ target/cucumber-htmlreport/report.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri('com\packtpub\mastering\selenium\AdvancedDocumentation.feature');
+formatter.feature({
+  "id": "verify-the-documentation-for-the-advanced-features-in-selenium",
+  "description": "\r\nIn order to show you a way to communicate with non technical stakeholder\r\nI want to introduce Cucumber in the video course \"Mastering Selenium Testing Tools\"",
+  "name": "Verify the documentation for the advanced features in Selenium",
+  "keyword": "Feature",
+  "line": 1
+});
+formatter.scenario({
+  "id": "verify-the-documentation-for-the-advanced-features-in-selenium;selenium-page-object-design-pattern",
+  "description": "",
+  "name": "Selenium Page Object Design Pattern",
+  "keyword": "Scenario",
+  "line": 6,
+  "type": "scenario"
+});
+formatter.step({
+  "name": "that seleniumhq.org is available",
+  "keyword": "Given ",
+  "line": 7
+});
+formatter.step({
+  "name": "I read the documentation about page objects",
+  "keyword": "When ",
+  "line": 8
+});
+formatter.step({
+  "name": "I should see the title \"Test Design Considerations — Selenium Documentation\" #Correct Title",
+  "keyword": "Then ",
+  "line": 9
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
